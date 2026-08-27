c23f3d21b4ae9d5f9587eb2548ceb8ccbc156978
JNLP <marco.delpretehome25@outlook.com>
2026-08-27 15:26:43 +0200

    Add files via upload

---
+++ RightNova/Program.cs
@@ -0,0 +1,12 @@
+namespace RightNova
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new RightNova());
+        }
+    }
+}
